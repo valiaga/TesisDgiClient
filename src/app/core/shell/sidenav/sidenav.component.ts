49f8521afea7e7fd387093e35c9467e155a29554
Vitmar Aliaga Cruz <vitmar.aliaga@lim.upeu.edu.pe>
2018-07-09 20:56:25 -0500

    save changes

--- src/app/core/shell/sidenav/sidenav.component.ts
+++ src/app/core/shell/sidenav/sidenav.component.ts
@@ -31,7 +31,7 @@ export class SidenavComponent implements OnInit {
       navItems: [
         { name: 'Lineas de investigación', icon: 'cached', route: 'linea-investigacions' },
         { name: 'Escuelas', icon: 'cached', route: 'escuelas' },
-        { name: 'Personas', icon: 'users', route: 'personas' },
+        { name: 'Personas', icon: 'account_box', route: 'personas' },
         { name: 'Configuración', icon: 'settings', route: 'config' },
         { name: 'Ayuda', icon: 'help', route: 'help' },
         { name: 'Usuarios', icon: 'beenhere', route: 'users' },
